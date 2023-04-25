dbe5dd03f24a81f7504c3e9aa71083b919697bf5
Carlos Caceres <carlosmicro33@gmail.com>
2023-04-25 13:23:22 -0500

    Comunicacion entre contenedores

--- backend/src/connection_db.js
+++ backend/src/connection_db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg')
 const pool = new Pool({
     user: 'pgadmin',
     password: 'pg123',
-    host: 'localhost',
+    host: 'postgres',
     port: 5432,
     database: 'mande_db'
 })
