a5a79335f877abb9c08acae61dc530c291b99751
Santiago Gutierrez Hincapie <gutierrez.santiago@correounivalle.edu.co>
2023-04-26 17:39:47 -0500

    Se avanza en el login usando materials UI

--- forme-app/src/pages/login.js
+++ forme-app/src/pages/login.js
@@ -1,45 +1,65 @@
 import React, { useState } from "react";
-// import "../styles/login.css";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000000',
+    },
+    secondary: {
+      main: '#F6BE00',
+    },    
+  },
+});
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Email: ", email);
-    console.log("Password: ", password);
-  };
-
   return (
-    <div className="login-container">
-      <div className="app-name-container">
-        <h1>Forme</h1>
-      </div>
-      <div className="form-container">
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="password">contraseña:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit">Entrar</button>
-          <button type="button">Registrarme</button>
-        </form>
-      </div>
+    <div className="login-container" align="right">
+      <Stack 
+      direction="column"
+      alignItems="flex-end">
+          <ThemeProvider theme={theme}>
+            <Box align="center" sx={{
+                width: 300,
+                height: 500,
+                backgroundColor: "secondary.main",
+                p: 2,
+            }}>
+            <Typography variant="h4">LOGIN</Typography>
+            <br/> 
+              <div>
+                <TextField 
+                type="email" 
+                id="email" 
+                label="Email" 
+                variant="filled" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)}
+                />            
+              </div>
+              <br/>
+              <div>
+                <TextField 
+                type="password" 
+                id="password" 
+                label="Password" 
+                variant="filled" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <br/>
+              <Button variant="contained" type="submit" color="warning">Sign-in</Button>
+              <br/>              
+                <Button type="button" color="primary">Register</Button>
+            </Box>
+          </ThemeProvider>
+      </Stack>
     </div>
   );
 };
