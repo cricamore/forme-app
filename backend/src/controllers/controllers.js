9742cd58a9fd9b4967c8ca4b113e32c9e40146a5
Santi4go32 <quierocarne70@gmail.com>
2023-04-26 19:05:55 -0500

    rating semi backend added

--- backend/src/controllers/controllers.js
+++ backend/src/controllers/controllers.js
@@ -1,8 +1,10 @@
 const pool = require('../connection_db')
 
 const getTrabajador = async (req, res , next) => {
+    const idTrabajador = req.params.id_trabajador;
     const { cedula, password } = req.body
     try {
+        const idTrabajador = req.params.id_trabajador;
         let sql = `SELECT * FROM Persona NATURAL JOIN Trabajador AS t WHERE t.cedula='${cedula}' AND password='${password}';`
         const result = await pool.query(sql)
         
@@ -11,13 +13,28 @@ const getTrabajador = async (req, res , next) => {
                 message:"Trabajador no encontrado"
             })
         }
-
+        
         res.json(result.rows)
     }catch (error) {
         next(error)
     }
 }
 
+const updateEstrellas = async (req, res, next) => {
+  const { estrellas } = req.body;
+  const { id_trabajador } = req.params;
+
+  try {
+    let sql = `UPDATE Trabajador SET estrellas=((${estrellas}+estrellas)/2) 
+               WHERE id_trabajador IN (SELECT id_trabajador FROM Usuario_trabajador NATURAL JOIN Trabajador);`
+    const result = await pool.query(sql,[estrellas, id_trabajador]);
+    res.json(result.rows);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
+    updateEstrellas
     getTrabajador
 }
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
-const { getTrabajador } = require('../controllers/controllers.js')
+const { getTrabajador, updateEstrellas } = require('../controllers/controllers.js')
+
 
 
 const router = Router();
 
 router.post('/login', getTrabajador)
+router.put('/1005965561/estrellas', updateEstrellas)
 
 module.exports = router
