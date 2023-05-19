c2867794cdadc56361baff069694fe429a0a7aed
Camilo Montaño <cristian.camilo.montano@correounivalle.edu.co>
2023-05-19 00:03:10 -0500

    backend tests fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,27 @@
-// Archivo que arranca server
-// Correr server con Npm run dev
+// Archivo principal (index.js)
 
-const express = require('express')
-const morgan = require('morgan')
-const routes = require('./routes/routes.js')
-const app = express()
-const cors = require('cors')
+const express = require('express');
+const morgan = require('morgan');
+const routes = require('./routes/routes.js');
+const app = express();
+const cors = require('cors');
 
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(routes)
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(routes);
 app.use((err, req, res, next) => {
     return res.json({
         message: err.message
-    })
-})
-module.exports = app
-app.listen(4000)
-console.log('Server estoy en port 4000')
+    });
+});
+
+let server; // Variable para almacenar el servidor
+
+if (process.env.NODE_ENV !== 'test') {
+  server = app.listen(4000, () => {
+    console.log('Server estoy en port 4000');
+  });
+}
+
+module.exports = server ? server : app;
